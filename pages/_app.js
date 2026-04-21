2838046fe2de17f97edd5c3b836517c3bd54ed08
PATEL ABHAY RAJKUMAR <patelabhay6980@gmail.com>
2026-04-21 10:41:14 +0000

    Add Google Analytics tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,23 @@
 import Layout from "@/components/common/Layout"
+import Script from "next/script"
 import "@/styles/main.scss"
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-2NECK0FWZ6" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-2NECK0FWZ6');
+        `}
+      </Script>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
